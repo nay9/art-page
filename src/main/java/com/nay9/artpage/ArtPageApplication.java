02de1b83760ab511e451a3df529c8880d3170ba9
Rene G <renenoe94@hotmail.com>
2019-03-06 16:33:38 -0500

    first commit

---
+++ src/main/java/com/nay9/artpage/ArtPageApplication.java
@@ -0,0 +1,13 @@
+package com.nay9.artpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtPageApplication.class, args);
+	}
+
+}
